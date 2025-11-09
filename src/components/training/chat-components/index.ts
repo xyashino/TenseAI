60e89e184dc02c3c90b309b1d904fe787196e1fc
Szczepan Micek <micek.szczepan.w@gmail.com>
2025-11-09 18:12:32 +0100

    feat: introduce WelcomeCard component for personalized user greeting and training session details, along with new types for component props

---
+++ src/components/training/chat-components/index.ts
@@ -0,0 +1,5 @@
+export { FinalFeedbackElement } from "./final-feedback-element";
+export { LoadingElement } from "./loading-element";
+export { RoundSummaryElement } from "./round-summary-element";
+export { SelectQuestionForm as SelectQuestionList } from "./select-question-list/select-question-list-form";
+export * from "./types";
