6ab204d038e535607bce82f159be8b6caf6907a9
Szczepan Micek <micek.szczepan.w@gmail.com>
2025-11-10 12:40:13 +0100

    style: adjust button width in FinalFeedbackActions for improved responsiveness

--- src/components/training/chat-components/final-feedback-element/final-feedback-actions.tsx
+++ src/components/training/chat-components/final-feedback-element/final-feedback-actions.tsx
@@ -6,13 +6,13 @@ import { memo } from "react";
 export const FinalFeedbackActions = memo(function FinalFeedbackActions() {
   return (
     <div className="flex flex-col sm:flex-row gap-3 pt-4">
-      <Button variant="default" className="flex-1" size="lg" asChild>
+      <Button variant="default" className="w-full sm:flex-1" size="lg" asChild>
         <a href={NavigationRoutes.HISTORY}>
           <History className="h-4 w-4 mr-2" />
           View History
         </a>
       </Button>
-      <Button variant="outline" className="flex-1" size="lg" asChild>
+      <Button variant="outline" className="w-full sm:flex-1" size="lg" asChild>
         <a href={NavigationRoutes.TRAINING}>
           <PlayIcon className="h-4 w-4 mr-2" />
           Start New Session
