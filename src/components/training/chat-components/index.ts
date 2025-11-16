62a115b93bf7e0d001170ca9015e975f9ae9cf7d
Szczepan Micek <micek.szczepan.w@gmail.com>
2025-11-16 13:15:22 +0100

    feat: add read-only round summary component and update chat component renderer

--- src/components/training/chat-components/index.ts
+++ src/components/training/chat-components/index.ts
@@ -1,4 +1,5 @@
 export { FinalFeedbackElement } from "./final-feedback-element";
 export { LoadingElement } from "./loading-element";
 export { RoundSummaryElement } from "./round-summary-element";
+export { RoundSummaryElementReadOnly } from "./round-summary-element-read-only";
 export { SelectQuestionForm as SelectQuestionList } from "./select-question-list/select-question-list-form";
